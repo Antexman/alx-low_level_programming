1e170c3121d777b12b80d37e7101674f12768d72
Antexman <anteneh.a@hotmail.com>
2022-12-08 21:55:16 +0000

    Hot fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,10 @@
  */
 int main(void)
 {
-int integerType;
-char charType;
-float floatType;
-long int longintType;
-long long int longlongintType;
-
-printf("Size of a char: %ld byte(s)\n", sizeof(charType));
-printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
-printf("Size of a long int: %ld byte(s)\n", sizeof(longintType));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintType));
-printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
-
-return (0);
+          printf("Size of a char: %ld byte(s)\n", sizeof(char));
+          printf("Size of an int: %ld byte(s)\n", sizeof(int));
+          printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+          printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+          printf("Size of a float: %ld byte(s)\n", sizeof(float));
+          return (0);
 }
