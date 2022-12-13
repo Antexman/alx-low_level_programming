51f3117d474b728357b5d183506c47fb23af0387
Antexman <anteneh.a@hotmail.com>
2022-12-13 16:14:44 +0000

    Task Hot fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,45 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_to_98 - print
+ * print_to_98 - print all natural numbers
+ * @n: natural number starting point
  *
- * Description: prints all natural numbers
- *
- * Return: 0
- * @n:input
+ * return: returns nothing
  */
-
 void print_to_98(int n)
 {
-int a, b, c;
-if (n <= 15 && n >= 0)
+if (n <= 98)
 {
-for (a = 0; a <= n; a++)
+for (n = n; n <= 97; n++)
 {
-_putchar('0');
-
-for (b = 1; b <= n; b++)
-{
-_putchar(',');
-_putchar(' ');
-c = a * b;
-if (c <= 99)
-_putchar(' ');
-
-if ( c <= 9)
-_putchar(' ');
-if (c >= 100);
-{
-_putchar((c / 100) + '0');
-_putchar((c / 10) % 10 + '0');
-}
-else if (c <= 99 && c >= 10)
-{
-_putchar((c / 10) + '0');
-}
-_putchar((c % 10) + '0');
-}
-_putchar('\n');
+printf("%d, ", n);
 }
 }
+else
+{
+for (n = n; n > 98; n--)
+{
+printf("%d, ", n);
+}
+}
+printf("98\n");
 }
