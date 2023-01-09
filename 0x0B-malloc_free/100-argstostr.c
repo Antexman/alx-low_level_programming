65ae6bf2b55bbf768d1a9dcb0922c31e0f82aedd
Antexman <anteneh.a@hotmail.com>
2023-01-09 09:55:44 +0000

    Hot Fix 2 Task 6

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,51 +10,36 @@
 * @av: array
 * Return: a pointer to a new string or NULL if it fails
 */
-
 char *argstostr(int ac, char **av)
 {
-char *s = NULL;
-int n = 0, m = ac, p, i = 0, j = 0;
-if (ac == 0 || av == NULL)
-{
-return (NULL);
-}
-while (ac--)
-i += (mblen(av[ac]) + 1);
-s = (char *) malloc(i + 1);
+	int i, n, k = 0, len = 0;
+	char *str;
 
-if (s != NULL)
-{
-while (n < m)
-{
-for (p = 0; av[n][p] != '\0'; p++)
-s[p + j] = av[n][p];
-s[j + p] = '\n';
-j += (p + 1);
-n++;
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-}
-s[j] = '\0';
-}
-else
-{
-return (NULL);
-}
-return (s);
-}
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			len++;
+	}
+	len += ac;
 
-/**
-* len: length
-* @str:string
-* Return: len
-*/
-int len(char *str)
-{
-int len = 0;
-if (str != NULL)
-{
-while (str[len])
-len++;
-}
-return (len);
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+		{
+			str[k] = av[i][n];
+			k++;
+		}
+		if (str[k] == '\0')
+		{
+			str[k++] = '\n';
+		}
+	}
+	return (str);
 }
